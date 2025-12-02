906ec6e87b928984250a9470e81ad321e2d9f5e7
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-12-02 22:33:03 +0100

    feat: update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAH3jW-eroMvPUWoR6gBaYQ0UI0HU3oAxA",
+  apiKey: import.meta.env.VITE_API_FIREBASE_KEY,
   authDomain: "fluencyportal-c01c1.firebaseapp.com",
   databaseURL: "https://fluencyportal-c01c1-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "fluencyportal-c01c1",
   storageBucket: "fluencyportal-c01c1.firebasestorage.app",
   messagingSenderId: "418675787644",
-  appId: "1:418675787644:web:638db57eb47d6aa42a86f9"
+  appId: "1:418675787644:web:0f097116cdce287b2a86f9"
 };
 
 const app = initializeApp(firebaseConfig);
