ac6c266dd27b3a9c5184dbfaa653b29052a67d91
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-12-02 21:13:50 +0100

    feat: add WelcomePage logic with temporary test data
    - added imports for Header and TodayActivity components
    - connected useCoursesStore and useTeachersStore
    - added fetch data
    - added useMemo to filter courses by studentId and date
    - used temporary test values for id and selectedDate

--- src/pages/welcomePage/WelcomePage.tsx
+++ src/pages/welcomePage/WelcomePage.tsx
@@ -1,9 +1,41 @@
 import {Container} from "@mui/material";
+import {Header} from "../../components/header/Header.tsx";
+import {TodayActivity} from "./TodayActivity.tsx";
+import {useEffect, useMemo} from "react";
+import {useCoursesStore} from "../../stores/useCoursesStore.ts";
+import {useTeachersStore} from "../../stores/useTeachersStore.ts";
+import {isSameDay} from "date-fns";
 
 export function WelcomePage() {
+  const id = 0;
+  const selectedDate = new Date("2024-01-16");
+
+  const courses = useCoursesStore(state => state.courses);
+  const fetchTeachers = useTeachersStore(state => state.fetchTeachers);
+  const fetchCourses = useCoursesStore(state => state.fetchCourses);
+
+  useEffect(() => {
+    fetchCourses();
+    fetchTeachers();
+  }, [fetchCourses, fetchTeachers]);
+
+  const filtered = useMemo(
+    () => courses.filter(course => course.studentIds.includes(id))
+      .filter(course =>
+        isSameDay(new Date(course.startTime), selectedDate)
+      ),
+    [courses, selectedDate]
+  );
+
   return (
     <>
-      <Container>
+      <Header/>
+
+      <Container
+        maxWidth={"xl"}
+
+      >
+        <TodayActivity courses={filtered}/>
 
       </Container>
     </>
