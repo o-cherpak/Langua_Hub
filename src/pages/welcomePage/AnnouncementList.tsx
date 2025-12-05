4c41ec84fe44b684e4bf285f420e2c6341e21f57
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-12-05 23:44:15 +0100

    refactor: update AnnouncementList.tsx
    - update width
    - increase length of announcements

--- src/pages/welcomePage/AnnouncementList.tsx
+++ src/pages/welcomePage/AnnouncementList.tsx
@@ -1,15 +1,16 @@
-import {useAnnouncementsStore} from "../../stores/useAnnouncementsStore.ts";
-import {List} from "@mui/material";
-import {AnnouncementItem} from "../../components/AnnouncementItem.tsx";
+import { useAnnouncementsStore } from "../../stores/useAnnouncementsStore.ts";
+import { List} from "@mui/material";
+import { AnnouncementItem } from "../../components/AnnouncementItem.tsx";
 
 export function AnnouncementList() {
-  const announcements = useAnnouncementsStore(state => state.announcements);
-  const latestAnnouncements = announcements.slice(0,3);
+  const announcements = useAnnouncementsStore((state) => state.announcements);
+  const latestAnnouncements = announcements.slice(0, 5);
+
 
   return (
-    <List sx={{display: "flex", flexDirection: "column", width: "36%"}}>
-      {latestAnnouncements && latestAnnouncements.map((announcement) => (
-        <AnnouncementItem data={announcement} key={announcement.id}/>
+    <List sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
+      {latestAnnouncements.map((announcement) => (
+        <AnnouncementItem data={announcement} key={announcement.id} />
       ))}
     </List>
   );
