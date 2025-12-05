701eea04b193a5d276af5151932e0eb87abb8d91
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-12-05 20:58:35 +0100

    feat: add IAnnouncement interface

--- src/interfaces/ILanguageSchoolDatabase.ts
+++ src/interfaces/ILanguageSchoolDatabase.ts
@@ -1,9 +1,11 @@
 import type {IStudent} from "./IStudent.ts";
 import type {ITeacher} from "./ITeacher.ts";
 import type {ICourse} from "./ICourse.ts";
+import type {IAnnouncement} from "./IAnnouncement.ts";
 
 export interface ILanguageSchoolDatabase {
   students:IStudent[];
   teachers:ITeacher[];
   courses: ICourse[];
+  announcements: IAnnouncement[];
 }
