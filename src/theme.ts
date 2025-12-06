a5ae0db634efe3abcd8686aad6d013ca7bade7b6
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-12-06 21:16:02 +0100

    init: create theme.ts and add font links

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import {createTheme} from '@mui/material/styles';
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: '"Inter", sans-serif',
+
+    h1: { fontFamily: '"Poppins", sans-serif' },
+    h2: { fontFamily: '"Poppins", sans-serif' },
+    h3: { fontFamily: '"Poppins", sans-serif' },
+    h4: { fontFamily: '"Poppins", sans-serif' },
+    h5: { fontFamily: '"Poppins", sans-serif', fontWeight: 700 },
+    h6: { fontFamily: '"Poppins", sans-serif', fontWeight: 600 },
+  }
+});
