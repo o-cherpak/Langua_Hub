eda3d18077401815c61ac3c6e713800f4e0fd8cc
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-12-06 21:17:30 +0100

    init: add ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import {WelcomePage} from "./pages/welcomePage/WelcomePage.tsx";
+import {ThemeProvider} from "@mui/material";
+import {theme} from "./theme.ts";
 
 function App() {
   return (
-    <>
-    </>
+    <ThemeProvider theme={theme}>
+      <WelcomePage/>
+    </ThemeProvider>
   )
 }
 
