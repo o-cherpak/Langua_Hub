22cf4e6d086b69d3ee6bc0a5cd3d0575983ee835
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-12-08 01:48:56 +0100

    feat: use ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import {RouterProvider} from "react-router";
 import {router} from "./router.tsx";
+import { ThemeProvider } from '@emotion/react';
+import {theme} from "./theme.ts";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router}/>
+    </ThemeProvider>
   </StrictMode>,
 )
